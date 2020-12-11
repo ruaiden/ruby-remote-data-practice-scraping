26cd87fc3118f76417821d8a2c87d1ebb28f6406
Aiden <aiden.marques94@gmail.com>
2020-12-11 16:47:24 -0500

    commit tree 4a57603d8fcfcaf65eadfb5f915b8e1e8e062559

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,16 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+doc.css(".headline-26OIBN").text.strip
+
+courses = doc.css(".title-oE5vT4")
+doc.css(".title-oE5vT4")[0]
+doc.css(".title-oE5vT4")[0].name
+doc.css(".title-oE5vT4")[0].attributes 
+
+courses.each do |course|
+  puts course.text.strip
+end 
